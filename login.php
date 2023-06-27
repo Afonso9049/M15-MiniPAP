91ab59967094a5fb553336b4e4397227844f2b87
Afonso9049 <a9049@oficina.pt>
2023-06-27 10:23:30 +0100

    Co-authored-by: Rafael Costa <9012Rafael@users.noreply.github.com>

--- login.php
+++ login.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="utf-8"/>
+    <meta charset="utf-8" />
     <title>Login</title>
-    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="style.css" />
 </head>
+
 <body>
-<?php
+    <?php
     require('db.php');
     session_start();
     // When form submitted, check and create user session.
     if (isset($_POST['username'])) {
-        $username = stripslashes($_REQUEST['username']);    // removes backslashes
+        $username = stripslashes($_REQUEST['username']); // removes backslashes
         $username = htmlspecialchars($username, ENT_QUOTES, 'UTF-8');
         $password = stripslashes($_REQUEST['password']);
         $password = htmlspecialchars($password, ENT_QUOTES, 'UTF-8');
@@ -22,27 +24,31 @@
         $stmt->bindParam(':password', md5($password), PDO::PARAM_STR);
         $stmt->execute();
         $rows = $stmt->rowCount();
-        if ($rows == 1) {
-            $_SESSION['username'] = $username;
-            // Redirect to user dashboard page
-            header("Location: dashboard.php");
+        // Check if user is admin based on their username
+        if ($username == "root") {
+            $user['role'] = "admin";
         } else {
-            echo "<div class='form'>
-                  <h3>Incorrect Username/password.</h3><br/>
-                  <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
-                  </div>";
+            $user['role'] = "user";
+        }
+
+        // Redirect the user to the appropriate page based on their role
+        if ($user['role'] == 'admin') {
+            header("Location: admin_page.php");
+        } else {
+            header("Location: user_page.php");
         }
     } else {
-?>
-    <form class="form" method="post" name="login">
-        <h1 class="login-title">Login</h1>
-        <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
-        <input type="password" class="login-input" name="password" placeholder="Password"/>
-        <input type="submit" value="Login" name="submit" class="login-button"/>
-        <p class="link"><a href="registration.php">New Registration</a></p>
-  </form>
-<?php
+        ?>
+        <form class="form" method="post" name="login">
+            <h1 class="login-title">Login</h1>
+            <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true" />
+            <input type="password" class="login-input" name="password" placeholder="Password" />
+            <input type="submit" value="Login" name="submit" class="login-button" />
+            <p class="link"><a href="registration.php">New Registration</a></p>
+        </form>
+        <?php
     }
-?>
+    ?>
 </body>
+
 </html>
--- registration.php
+++ registration.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="utf-8"/>
+    <meta charset="utf-8" />
     <title>Registration</title>
-    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="style.css" />
 </head>
+
 <body>
-<?php
+    <?php
     require('db.php');
     if (isset($_REQUEST['username'])) {
         // Removes backslashes and escapes special characters in a string
@@ -49,17 +51,18 @@
                   </div>";
         }
     } else {
-?>
-    <form class="form" action="" method="post">
-        <h1 class="login-title">Registration</h1>
-        <input type="text" class="login-input" name="username" placeholder="Username" required />
-        <input type="text" class="login-input" name="email" placeholder="Email Adress">
-        <input type="password" class="login-input" name="password" placeholder="Password">
-        <input type="submit" name="submit" value="Register" class="login-button">
-        <p class="link"><a href="login.php">Click to Login</a></p>
-    </form>
-<?php
+        ?>
+        <form class="form" action="" method="post">
+            <h1 class="login-title">Registration</h1>
+            <input type="text" class="login-input" name="username" placeholder="Username" required />
+            <input type="text" class="login-input" name="email" placeholder="Email Adress">
+            <input type="password" class="login-input" name="password" placeholder="Password">
+            <input type="submit" name="submit" value="Register" class="login-button">
+            <p class="link"><a href="login.php">Click to Login</a></p>
+        </form>
+        <?php
     }
-?>
+    ?>
 </body>
+
 </html>
